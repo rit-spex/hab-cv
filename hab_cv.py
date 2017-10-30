acbaee852df846e10a57073ccea6445597ed03e8
runphilrun <pjl7651@rit.edu>
2017-10-29 21:59:00 -0700

    multithreaded capture

--- hab_cv.py
+++ hab_cv.py
@@ -1,37 +1,74 @@
 from __future__ import print_function
 from imutils.video.pivideostream import PiVideoStream
-from imutils.video import FPS
+from imutils.convenience import resize
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import numpy as np
-import imutils
+import datetime
 import time
 import cv2
-import numpy
+import numpy as np
+import threading
+import queue
+import logging
 
-def smooth(img, radius, kind='open'):
-    kernel = np.ones((radius, radius), np.uint8)
-    if kind == 'gaussian':
-        img = cv2.GaussianBlur(img, (radius, radius), 0)
-    elif kind == 'open':
-        img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-    elif kind == 'close':
-        img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
-    elif kind == 'erode':
-        img = cv2.erode(mask, kernel, iterations=1)
-    elif kind == 'dilate':
-        img = cv2.dilate(mask, kernel, iterations=1)
-    return img
+class FrameReader():
+    def __init__(self):
+        super(FrameReader,self).__init__()
+        self.stopped = True
 
-def hsl_mask(img, limits, radius=1):
+    def run(self):
+        while not self.stopped:
+            if not q.full():
+                frame=stream.read()
+                framename=datetime.datetime.now().time().strftime("%Y%m%d-%H%M%S")
+                cv2.imwrite('raw/'+framename+'.jpg',frame)
+                q.put((frame,framename))
+            else: logging.debug('Queue is full ('+str(q.qsize())+'frames)')
+        return
+
+    def start(self):
+        self.stopped = False
+        t = threading.Thread(target=self.run, args=(), name='reader')
+        t.daemon = True
+        t.start()
+
+    def stop(self):
+        self.stopped = True
+        logging.debug('stopped FrameReader')
+
+class FrameMasker(threading.Thread):
+    def __init__(self):
+        super(FrameMasker,self).__init__()
+        self.stopped = True
+
+    def run(self):
+        logging.debug('started FrameMasker')
+        while not self.stopped:
+            if not q.empty():
+                (frame, framename) = q.get()
+                frame = resize(frame, width=300)
+                mask = get_hsl_mask(frame)
+                cv2.imwrite('mask/'+framename+'.jpg',mask)
+            else: logging.debug('Queue is empty!')
+
+    def start(self):
+        self.stopped = False
+        t = threading.Thread(target=self.run, args=(), name='masker')
+        t.daemon = True
+        t.start()
+
+    def stop(self):
+        self.stopped = True
+        logging.debug('stopped FrameMasker')
+
+def hsl_mask(img, limits):
     hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS_FULL)
     lower_limit, upper_limit = np.array(limits[0], dtype='uint8'), np.array(limits[1], dtype='uint8')
     mask = cv2.inRange(hls, lower_limit, upper_limit)
-    if radius >1: mask = smooth(mask, radius, kind='close')
-    masked_img = cv2.bitwise_and(img, img, mask=mask)
-    return mask, masked_img
+    return mask
 
-def get_hsl_mask(img, selection='vegetation', smoothing=0):
+def get_hsl_mask(img, selection='vegetation'):
     # LIMITS IN HLS - Hue, Lightness, Saturation
     # range: 0 to 255
     # SEE: color_previewer()
@@ -39,8 +76,8 @@ def get_hsl_mask(img, selection='vegetation', smoothing=0):
               'water': ([120, 35, 20], [180, 175, 255]),
               'urban': ([0, 0, 0], [255, 255, 20])
               }
-    mask, masked_img = hsl_mask(img, ranges[selection], smoothing)
-    return mask, masked_img
+    mask = hsl_mask(img, ranges[selection])
+    return mask
 
 # def ndvi(img_color, img_nir):
 #     nir = cv2.cvtColor(img_nir, cv2.COLOR_RGB2GRAY)
@@ -52,26 +89,27 @@ def get_hsl_mask(img, selection='vegetation', smoothing=0):
 #
 #     return np.divide(num, den)
 
-print("[INFO] started threaded video stream")
-vs = PiVideoStream(framerate=60).start()
-time.sleep(2.0)
-fps = FPS()
-fps.start()
-while fps._numFrames < 500:
-	frame = vs.read()
-	# frame = imutils.resize(frame, width=400)
-	mask, masked_img = get_hsl_mask(frame)
-	# cv2.imshow('', masked_img)
-	# if cv2.waitKey(25) & 0xFF == ord('q'):
-	# 	cv2.destroyAllWindows()
-	# 	break
-	fps.update()
+def stopAll(stream,reader,masker):
+    reader.stop()
+    masker.stop()
+    stream.stop()
+    return
 
-fps.stop()
-print("[INFO] frames sampled: {}".format(str(fps._numFrames)))
-print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-print("[INFO] avg. FPS: {:.2f}".format(fps.fps()))
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG,
+                        format='(%(threadName)-9s) %(message)s',)
 
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+    q = queue.Queue()
+    resolution=(640,480) # http://picamera.readthedocs.io/en/release-1.10/fov.html
+    framerate=60
+    logging.debug('started threaded video stream ('+str(resolution)+','+str(framerate)+' fps)')
+    stream = PiVideoStream(resolution=resolution,framerate=framerate).start()
+    for i in reversed(range(1,6)):
+        logging.debug('starting capture & processing in '+str(i)+' seconds')
+        time.sleep(1)
+    reader,masker=FrameReader(),FrameMasker()
+    reader.start()
+    masker.start()
+
+    time.sleep(10)
+    stopAll(stream,reader,masker)
