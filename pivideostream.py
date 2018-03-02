6aab88952fe62f7d945daa81a5ec8ccaab29c8c3
Philip Linden <runphilrun@users.noreply.github.com>
2018-03-02 11:12:54 -0800

    make video stream multithread safe

--- pivideostream.py
+++ pivideostream.py
@@ -8,50 +8,51 @@
 # import the necessary packages
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-from threading import Thread
+import threading
 
 class PiVideoStream:
-	def __init__(self, resolution=(320, 240), framerate=32):
-		# initialize the camera and stream
-		self.camera = PiCamera()
-		self.camera.resolution = resolution
-		self.camera.framerate = framerate
-		self.rawCapture = PiRGBArray(self.camera, size=resolution)
-		self.stream = self.camera.capture_continuous(self.rawCapture,
-			format="rgb", use_video_port=True)
+    def __init__(self, resolution=(320, 240), framerate=32):
+        # initialize the camera and stream
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.framerate = framerate
+        self.rawCapture = PiRGBArray(self.camera, size=resolution)
+        self.stream = self.camera.capture_continuous(self.rawCapture,
+            format="rgb", use_video_port=True)
+        self.lock = threading.Lock() # thread safe lock
+        # initialize the frame and the variable used to indicate
+        # if the thread should be stopped
+        self.frame = None
+        self.stopped = False
 
-		# initialize the frame and the variable used to indicate
-		# if the thread should be stopped
-		self.frame = None
-		self.stopped = False
+    def start(self):
+        # start the thread to read frames from the video stream
+        self.t = threading.Thread(target=self.update, args=())
+        self.t.daemon = True
+        self.t.start()
+        return self
 
-	def start(self):
-		# start the thread to read frames from the video stream
-		t = Thread(target=self.update, args=())
-		t.daemon = True
-		t.start()
-		return self
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        with self.lock:
+            for f in self.stream:
+                # grab the frame from the stream and clear the stream in
+                # preparation for the next frame
+                self.frame = f.array
+                self.rawCapture.truncate(0)
 
-	def update(self):
-		# keep looping infinitely until the thread is stopped
-		for f in self.stream:
-			# grab the frame from the stream and clear the stream in
-			# preparation for the next frame
-			self.frame = f.array
-			self.rawCapture.truncate(0)
+                # if the thread indicator variable is set, stop the thread
+                # and resource camera resources
+                if self.stopped:
+                    self.stream.close()
+                    self.rawCapture.close()
+                    self.camera.close()
+                    return
 
-			# if the thread indicator variable is set, stop the thread
-			# and resource camera resources
-			if self.stopped:
-				self.stream.close()
-				self.rawCapture.close()
-				self.camera.close()
-				return
+    def read(self):
+        # return the frame most recently read
+        return self.frame
 
-	def read(self):
-		# return the frame most recently read
-		return self.frame
-
-	def stop(self):
-		# indicate that the thread should be stopped
-		self.stopped = True
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
