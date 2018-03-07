3257c683ff32fd2fb3c5f56cd2d3f0dd0b52bc99
runphilrun <pjl7651@rit.edu>
2018-03-07 01:40:37 -0800

    input args

--- wuap.py
+++ wuap.py
@@ -23,27 +23,36 @@ import threading # Parallelizing tasks
 import queue # Allow processing on captured frames as they are saved
 import logging # Log messages and information
 import os # Manage files and directories
+import argparse # Command line options
 
 class FrameReader():
     ''' Samples frames from a pi video stream and saves them to disk.
     '''
-    def __init__(self,stream,q,debug=False):
+    def __init__(self,stream,q,testmode=False):
         # super(FrameReader,self).__init__()
         self.stream = stream
         self.q = q
-        self.debug=debug
+        self.testmode=testmode
+        if testmode: logger.info('testing mode enabled!')
         self.stopped = True
 
     def run(self):
-        logging.debug('started FrameReader')
+        logger.debug('started FrameReader')
         while not self.stopped:
             frame=self.stream.read() # sample from stream
             framename=str(int(time.time()*10000000)) # name image based on time sampled
-            if self.debug: framename='TEST_'+framename
-            cv2.imwrite('raw/'+framename+'.jpg',frame) # save to disk as jpg
+            if self.testmode: framename='TEST_'+framename
+            try:
+                cv2.imwrite('raw/'+framename+'.jpg',frame) # save to disk as jpg
+            except Exception as e:
+                logger.error('failed to save image: '+'raw/raw_'+framename+'.jpg')
+                logger.error(e)
+                self.stop()
+                return
+            logger.debug('{0} saved and added to queue in position {1}.'.format('raw_'+framename,str(self.q.qsize())))
             if not self.q.full():
                 self.q.put((frame,framename)) # send frame to mask processing queue
-            else: logging.debug('Queue is full ('+str(self.q.qsize())+'frames)')
+            else: logger.debug('Queue is full! '+str(self.q.qsize())+' waiting...')
         return
 
     def start(self):
@@ -55,27 +64,42 @@ class FrameReader():
     def stop(self):
         self.stopped = True
         self.t.join()
-        logging.debug('stopped FrameReader')
+        logger.debug('stopped FrameReader')
 
 class FrameMasker(threading.Thread):
     ''' Masks frames from the queue and saves the mask to disk.
     '''
-    def __init__(self,stream,q,debug=False):
+    def __init__(self,stream,q,irmode=False,testmode=False):
         # super(FrameMasker,self).__init__()
         self.stream=stream
         self.q=q
-        self.debug=debug
+        self.testmode=testmode
+        if testmode:
+            logger.info('testing mode enabled!')
+            self.irmode=False
+        else:
+            self.irmode=irmode
+            if irmode: logger.info('infrared mode enabled!')
+
         self.stopped = True
 
     def run(self):
-        logging.debug('started FrameMasker')
+        logger.debug('started FrameMasker')
         while not self.stopped:
             if not self.q.empty():
                 (frame, framename) = self.q.get() # retrieve frame from queue (First In Last Out)
                 # frame = resize(frame, width=256)
                 mask = get_hls_mask(frame,self.debug) # get hls logical mask
-                cv2.imwrite('mask/'+framename+'.jpg',mask) # save image to disk
-            # else: logging.debug('Queue is empty!')
+                maskname = 'mask_'+framename
+                try:
+                    cv2.imwrite('mask/'+maskname+'.jpg',mask) # save image to disk
+                except Exception as e:
+                    logger.error('failed to save image: '+'raw/raw_'+framename+'.jpg')
+                    logger.error(e)
+                    self.stop()
+                    return
+                logger.debug('{0} saved. {1} items left in queue.'.format(maskname,str(self.q.qsize())))
+            else: logger.debug('Queue is empty, nothing to mask!')
 
     def start(self):
         self.stopped = False
@@ -84,12 +108,12 @@ class FrameMasker(threading.Thread):
         self.t.start()
 
     def stop(self):
-        logging.debug('masking remaining frames...')
+        logger.debug('masking remaining frames before stopping...')
         while(True):
             if self.q.empty():
                 self.stopped = True
                 self.t.join()
-                logging.debug('stopped FrameMasker')
+                logger.debug('stopped FrameMasker')
                 break
 
 def hls_mask(img, limits):
@@ -103,14 +127,16 @@ def hls_mask(img, limits):
     mask = cv2.inRange(hls, lower_limit, upper_limit)
     return mask
 
-def get_hls_mask(img,debug=False):
+def get_hls_mask(img,irmode=False,testmode=False):
     ''' Generate a binary logical mask to filter by a Hue-Saturation-Value range.
         Hue [0 255]
         Saturation [0 255]
         Value [0 255]
     '''
-    if debug:
-        color_range = ([0,35,35], [100,100,100])
+    if testmode:
+        color_range = ([0,0,255/2.], [255,255/2.,255])
+    elif irmode:
+        color_range = ([0,0,0], [255,255,255])
     else:
         color_range = ([35, 5, 35], [120, 140, 255])
     mask, mask_3channel = hls_mask(img, color_range)
@@ -122,99 +148,159 @@ def stopAll(stream,reader,masker):
     masker.stop()
     return
 
-def init():
+def init(refresh=False):
     ''' Run hardware and software system checks.
     '''
-    logging.basicConfig(level=logging.DEBUG,
-                        format='(%(threadName)-9s) %(message)s',)
-    logging.debug(' --- Where U At Plants? --- ')
-    logging.debug('performing startup and system checks:')
+    logger.debug('performing startup and system checks')
     try:
         # check for directory structure
         if os.path.exists('raw'):
-            logging.debug('  found directory: raw')
+            logger.debug('found directory: raw')
+            if refresh:
+                logger.debug('(refresh enabled: removing existing contents)')
+                filelist = [ f for f in os.listdir('raw') ]
+                for f in filelist:
+                    try:
+                        os.remove(os.path.join('raw', f))
+                    except Exception as e:
+                        logger.error(e)
+                        return False
         else:
             os.mkdir('raw')
-            logging.debug('  created directory: raw')
+            logger.debug('created directory: raw')
+    except:
+        logger.critical('[NO GO] raw file directory')
+        return False
+
+    try:
         if os.path.exists('mask'):
-            logging.debug('  found directory: mask')
+            logger.debug('found directory: mask')
+            if refresh:
+                logger.debug('(refresh enabled: removing existing contents)')
+                filelist = [ f for f in os.listdir('mask') ]
+                for f in filelist:
+                    try:
+                        os.remove(os.path.join('mask', f))
+                    except Exception as e:
+                        logger.error(e)
+                        return False
         else:
             os.mkdir('mask')
-            logging.debug('  created directory: mask')
+            logger.debug('created directory: mask')
     except:
-        logging.debug('[NO GO] file directories')
+        logger.critical('[NO GO] mask file directory')
         return False
-    logging.debug('[GO] file directories')
+    logger.info('[GO] file directories')
 
     try:
         # check video stream
         teststream = PiVideoStream(resolution=(640,480),framerate=60).start()
-        logging.debug('  (spooling PiVideoStream...)')
+        logger.debug('(spooling PiVideoStream...)')
         time.sleep(2)
         testframe = teststream.read()
         teststream.stop()
     except:
         teststream.stop()
-        logging.debug('[NO GO] PiVideoStream')
+        logger.critical('[NO GO] PiVideoStream')
         return False
-    logging.debug('[GO] PiVideoStream')
-    logging.debug('  (restarting PiVideoStream...)')
+    logger.info('[GO] PiVideoStream')
+    logger.debug('(restarting PiVideoStream...)')
     teststream.start()
     time.sleep(2)
     try:
         # check FrameReader
         testq=queue.Queue()
-        testreader,testmasker = FrameReader(stream=teststream,q=testq,debug=True), FrameMasker(stream=teststream,q=testq,debug=True)
-        logging.debug('  (spooling FrameReader...)')
+        testreader,testmasker = FrameReader(stream=teststream,q=testq,testmode=True), FrameMasker(stream=teststream,q=testq,testmode=True)
+        logger.debug('(spooling FrameReader...)')
         testreader.start()
         time.sleep(1)
         testreader.stop()
     except:
         testreader.stop()
-        logging.debug('[NO GO] FrameReader')
+        logger.critical('[NO GO] FrameReader')
         return False
-    logging.debug('[GO] FrameReader')
+    logger.info('[GO] FrameReader')
     teststream.stop()
 
     try:
         # check FrameMasker
-        logging.debug('  (spooling FrameMasker...)')
+        logger.debug('(spooling FrameMasker...)')
         testmasker.start()
         time.sleep(1)
         testmasker.stop()
     except:
-        logging.debug('[NO GO] FrameMasker')
+        logger.critical('[NO GO] FrameMasker')
         testmasker.stop()
         return False
-    logging.debug('[GO] FrameMasker')
+    logger.info('[GO] FrameMasker')
 
-    logging.debug('All systems go!')
+    logger.info('All systems go!')
     return True
 
 if __name__ == '__main__':
-    if not init():
-        logging.debug('ABORTING STARTUP')
+    # options
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-q','--quiet',help='limited output to terminal, "INFO" items only',action='store_true')
+    parser.add_argument('-r','--refresh',help='delete existing images before startup',action='store_true')
+    parser.add_argument('-ir','--infrared',help='use alternate mask for PiCamera NoIR',action='store_true')
+    args = parser.parse_args()
+
+    LOG_FILE = os.path.splitext(__file__)[0]+'.log'
+
+    if args.refresh:
+        print('refresh mode enabled! clearing old log file before starting...')
+        try:
+            os.remove(LOG_FILE)
+        except Exception as e:
+            print(e)
+            print('ERROR DELETING LOG FILE. ABORTING...')
+            exit()
+
+    # initialize logger
+    logger= logging.getLogger(__name__)
+    logFormatter = logging.Formatter('%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s')
+    fileHandler = logging.FileHandler(LOG_FILE)
+    fileHandler.setFormatter(logFormatter)
+    logger.addHandler(fileHandler)
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    logger.addHandler(consoleHandler)
+    if args.quiet:
+        logger.setLevel(logging.INFO)
+    else:
+        logger.setLevel(logging.DEBUG)
+
+    logger.info(' --- Where U At Plants? --- ')
+    if not init(refresh=args.refresh):
+        logger.critical('ABORTING STARTUP')
         exit()
-    logging.debug('starting flight operations:')
+
+    logger.debug('starting flight operations:')
     q = queue.Queue()
     resolution=(640,480) # http://picamera.readthedocs.io/en/release-1.10/fov.html
     framerate=60
-    logging.debug('started threaded video stream ('+str(resolution)+','+str(framerate)+' fps)')
+    logger.debug('started threaded video stream ('+str(resolution)+','+str(framerate)+' fps)')
     stream = PiVideoStream(resolution=resolution,framerate=framerate).start()
     for i in reversed(range(1,4)):
-        logging.debug('starting capture & processing in '+str(i)+' seconds')
+        logger.debug('starting capture & processing in '+str(i)+' seconds')
         time.sleep(1)
-    reader,masker=FrameReader(stream=stream,q=q),FrameMasker(stream=stream,q=q)
+    reader,masker=FrameReader(stream=stream,q=q),FrameMasker(stream=stream,q=q,irmode=args.infrared)
     reader.start()
     masker.start()
-    logging.debug('Recording...')
-    while(True):
-        user_input = input('Stop the stream?: ')
+
+    print('Enter any character and press enter to stop the stream.')
+    logger.info('Recording...')
+    loopme=True
+    while(loopme):
         try:
+            user_input = input()
             if user_input:
-                logging.debug('ABORTING')
+                logger.critical('ABORTING')
                 stopAll(stream,reader,masker)
+                loopme=False
                 break
         except:
-            logging.debug('ABORTING')
+            logger.critical('ABORTING')
+            loopme=False
             stopAll(stream,reader,masker)
+            break
--- wuap_skimage.py
+++ wuap_skimage.py
@@ -21,27 +21,36 @@ import threading # Parallelizing tasks
 import queue # Allow processing on captured frames as they are saved
 import logging # Log messages and information
 import os # Manage files and directories
+import argparse # Command line options
 
 class FrameReader():
     ''' Samples frames from a pi video stream and saves them to disk.
     '''
-    def __init__(self,stream,q,debug=False):
+    def __init__(self,stream,q,testmode=False):
         # super(FrameReader,self).__init__()
         self.stream = stream
         self.q = q
-        self.debug=debug
+        self.testmode=testmode
+        if testmode: logger.info('testing mode enabled!')
         self.stopped = True
 
     def run(self):
-        logging.debug('started FrameReader')
+        logger.debug('started FrameReader')
         while not self.stopped:
             frame=self.stream.read() # sample from stream
             framename=str(int(time.time()*10000000)) # name image based on time sampled
-            if self.debug: framename='TEST_'+framename
-            io.imsave('raw/raw_'+framename+'.jpg',frame) # save to disk as jpg
+            if self.testmode: framename='TEST_'+framename
+            try:
+                io.imsave('raw/raw_'+framename+'.jpg',frame) # save to disk as jpg
+            except Exception as e:
+                logger.error('failed to save image: '+'raw/raw_'+framename+'.jpg')
+                logger.error(e)
+                self.stop()
+                return
+            logger.debug('{0} saved and added to queue in position {1}.'.format('raw_'+framename,str(self.q.qsize())))
             if not self.q.full():
-                self.q.put((frame,framename)) # send frame to mask processing queue 
-            else: logging.debug('Queue is full ('+str(self.q.qsize())+'frames)')
+                self.q.put((frame,framename)) # send frame to mask processing queue
+            else: logger.debug('Queue is full! '+str(self.q.qsize())+' waiting...')
         return
 
     def start(self):
@@ -53,27 +62,42 @@ class FrameReader():
     def stop(self):
         self.stopped = True
         self.t.join()
-        logging.debug('stopped FrameReader')
+        logger.debug('stopped FrameReader')
 
 class FrameMasker(threading.Thread):
     ''' Masks frames from the queue and saves the mask to disk.
     '''
-    def __init__(self,stream,q,debug=False):
+    def __init__(self,stream,q,irmode=False,testmode=False):
         # super(FrameMasker,self).__init__()
         self.stream=stream
         self.q=q
-        self.debug=debug
+        self.testmode=testmode
+        if testmode:
+            logger.info('testing mode enabled!')
+            self.irmode=False
+        else:
+            self.irmode=irmode
+            if irmode: logger.info('infrared mode enabled!')
+
         self.stopped = True
 
     def run(self):
-        logging.debug('started FrameMasker')
+        logger.debug('started FrameMasker')
         while not self.stopped:
             if not self.q.empty():
                 (frame, framename) = self.q.get() # retrieve frame from queue (First In Last Out)
                 # frame = resize(frame, width=256)
-                mask = get_hsv_mask(frame,self.debug) # get hsv logical mask
-                io.imsave('mask/mask_'+framename+'.jpg',mask) # save image to disk
-            # else: logging.debug('Queue is empty!')
+                mask = get_hsv_mask(frame,self.irmode,self.testmode) # get hsv logical mask
+                maskname = 'mask_'+framename
+                try:
+                    io.imsave('mask/'+maskname+'.jpg',mask) # save image to disk
+                except Exception as e:
+                    logger.error('failed to save image: '+'raw/raw_'+framename+'.jpg')
+                    logger.error(e)
+                    self.stop()
+                    return
+                logger.debug('{0} saved. {1} items left in queue.'.format(maskname,str(self.q.qsize())))
+            else: logger.debug('Queue is empty, nothing to mask!')
 
     def start(self):
         self.stopped = False
@@ -82,12 +106,12 @@ class FrameMasker(threading.Thread):
         self.t.start()
 
     def stop(self):
-        logging.debug('masking remaining frames...')
+        logger.debug('masking remaining frames before stopping...')
         while(True):
             if self.q.empty():
                 self.stopped = True
                 self.t.join()
-                logging.debug('stopped FrameMasker')
+                logger.debug('stopped FrameMasker')
                 break
 def hsv2decimal(hsv):
     ''' Convert integer values to decimal percentages
@@ -115,14 +139,16 @@ def hsv_mask(img, limits):
     mask=mask_3channel
     return mask, mask_3channel
 
-def get_hsv_mask(img,debug=False):
+def get_hsv_mask(img,irmode=False,testmode=False):
     ''' Generate a binary logical mask to filter by a Hue-Saturation-Value range.
         Hue [0 255]
         Saturation [0 100]
         Value [0 100]
     '''
-    if debug:
-        color_range = ([0,35,35], [100,100,100])
+    if testmode:
+        color_range = ([0,0,50], [100,50,100])
+    elif irmode:
+        color_range = ([0,0,0], [255,100,100])
     else:
         color_range = ([35, 50, 35], [120, 55, 100])
     mask, mask_3channel = hsv_mask(img, color_range)
@@ -134,99 +160,159 @@ def stopAll(stream,reader,masker):
     masker.stop()
     return
 
-def init():
+def init(refresh=False):
     ''' Run hardware and software system checks.
     '''
-    logging.basicConfig(level=logging.DEBUG,
-                        format='(%(threadName)-9s) %(message)s',)
-    logging.debug(' --- Where U At Plants? --- ')
-    logging.debug('performing startup and system checks:')
+    logger.debug('performing startup and system checks')
     try:
         # check for directory structure
         if os.path.exists('raw'):
-            logging.debug('  found directory: raw')
+            logger.debug('found directory: raw')
+            if refresh:
+                logger.debug('(refresh enabled: removing existing contents)')
+                filelist = [ f for f in os.listdir('raw') ]
+                for f in filelist:
+                    try:
+                        os.remove(os.path.join('raw', f))
+                    except Exception as e:
+                        logger.error(e)
+                        return False
         else:
             os.mkdir('raw')
-            logging.debug('  created directory: raw')
+            logger.debug('created directory: raw')
+    except:
+        logger.critical('[NO GO] raw file directory')
+        return False
+
+    try:
         if os.path.exists('mask'):
-            logging.debug('  found directory: mask')
+            logger.debug('found directory: mask')
+            if refresh:
+                logger.debug('(refresh enabled: removing existing contents)')
+                filelist = [ f for f in os.listdir('mask') ]
+                for f in filelist:
+                    try:
+                        os.remove(os.path.join('mask', f))
+                    except Exception as e:
+                        logger.error(e)
+                        return False
         else:
             os.mkdir('mask')
-            logging.debug('  created directory: mask')
+            logger.debug('created directory: mask')
     except:
-        logging.debug('[NO GO] file directories')
+        logger.critical('[NO GO] mask file directory')
         return False
-    logging.debug('[GO] file directories')
+    logger.info('[GO] file directories')
 
     try:
         # check video stream
         teststream = PiVideoStream(resolution=(640,480),framerate=60).start()
-        logging.debug('  (spooling PiVideoStream...)')
+        logger.debug('(spooling PiVideoStream...)')
         time.sleep(2)
         testframe = teststream.read()
         teststream.stop()
     except:
         teststream.stop()
-        logging.debug('[NO GO] PiVideoStream')
+        logger.critical('[NO GO] PiVideoStream')
         return False
-    logging.debug('[GO] PiVideoStream')
-    logging.debug('  (restarting PiVideoStream...)')
+    logger.info('[GO] PiVideoStream')
+    logger.debug('(restarting PiVideoStream...)')
     teststream.start()
     time.sleep(2)
     try:
         # check FrameReader
         testq=queue.Queue()
-        testreader,testmasker = FrameReader(stream=teststream,q=testq,debug=True), FrameMasker(stream=teststream,q=testq,debug=True)
-        logging.debug('  (spooling FrameReader...)')
+        testreader,testmasker = FrameReader(stream=teststream,q=testq,testmode=True), FrameMasker(stream=teststream,q=testq,testmode=True)
+        logger.debug('(spooling FrameReader...)')
         testreader.start()
         time.sleep(1)
         testreader.stop()
     except:
         testreader.stop()
-        logging.debug('[NO GO] FrameReader')
+        logger.critical('[NO GO] FrameReader')
         return False
-    logging.debug('[GO] FrameReader')
+    logger.info('[GO] FrameReader')
     teststream.stop()
 
     try:
         # check FrameMasker
-        logging.debug('  (spooling FrameMasker...)')
+        logger.debug('(spooling FrameMasker...)')
         testmasker.start()
         time.sleep(1)
         testmasker.stop()
     except:
-        logging.debug('[NO GO] FrameMasker')
+        logger.critical('[NO GO] FrameMasker')
         testmasker.stop()
         return False
-    logging.debug('[GO] FrameMasker')
+    logger.info('[GO] FrameMasker')
 
-    logging.debug('All systems go!')
+    logger.info('All systems go!')
     return True
 
 if __name__ == '__main__':
-    if not init():
-        logging.debug('ABORTING STARTUP')
+    # options
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-q','--quiet',help='limited output to terminal, "INFO" items only',action='store_true')
+    parser.add_argument('-r','--refresh',help='delete existing images before startup',action='store_true')
+    parser.add_argument('-ir','--infrared',help='use alternate mask for PiCamera NoIR',action='store_true')
+    args = parser.parse_args()
+
+    LOG_FILE = os.path.splitext(__file__)[0]+'.log'
+
+    if args.refresh:
+        print('refresh mode enabled! clearing old log file before starting...')
+        try:
+            os.remove(LOG_FILE)
+        except Exception as e:
+            print(e)
+            print('ERROR DELETING LOG FILE. ABORTING...')
+            exit()
+
+    # initialize logger
+    logger= logging.getLogger(__name__)
+    logFormatter = logging.Formatter('%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s')
+    fileHandler = logging.FileHandler(LOG_FILE)
+    fileHandler.setFormatter(logFormatter)
+    logger.addHandler(fileHandler)
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    logger.addHandler(consoleHandler)
+    if args.quiet:
+        logger.setLevel(logging.INFO)
+    else:
+        logger.setLevel(logging.DEBUG)
+
+    logger.info(' --- Where U At Plants? --- ')
+    if not init(refresh=args.refresh):
+        logger.critical('ABORTING STARTUP')
         exit()
-    logging.debug('starting flight operations:')
+
+    logger.debug('starting flight operations:')
     q = queue.Queue()
     resolution=(640,480) # http://picamera.readthedocs.io/en/release-1.10/fov.html
     framerate=60
-    logging.debug('started threaded video stream ('+str(resolution)+','+str(framerate)+' fps)')
+    logger.debug('started threaded video stream ('+str(resolution)+','+str(framerate)+' fps)')
     stream = PiVideoStream(resolution=resolution,framerate=framerate).start()
     for i in reversed(range(1,4)):
-        logging.debug('starting capture & processing in '+str(i)+' seconds')
+        logger.debug('starting capture & processing in '+str(i)+' seconds')
         time.sleep(1)
-    reader,masker=FrameReader(stream=stream,q=q),FrameMasker(stream=stream,q=q)
+    reader,masker=FrameReader(stream=stream,q=q),FrameMasker(stream=stream,q=q,irmode=args.infrared)
     reader.start()
     masker.start()
-    logging.debug('Recording...')
-    while(True):
-        user_input = input('Stop the stream?: ')
+
+    print('Enter any character and press enter to stop the stream.')
+    logger.info('Recording...')
+    loopme=True
+    while(loopme):
         try:
+            user_input = input()
             if user_input:
-                logging.debug('ABORTING')
+                logger.critical('ABORTING')
                 stopAll(stream,reader,masker)
+                loopme=False
                 break
         except:
-            logging.debug('ABORTING')
+            logger.critical('ABORTING')
+            loopme=False
             stopAll(stream,reader,masker)
+            break
