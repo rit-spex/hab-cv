0b521731ff85fea2aad0cef2dfbed9cf76d9aa96
runphilrun <pjl7651@rit.edu>
2018-03-11 13:18:28 -0700

    virtualenv first

--- installer.sh
+++ installer.sh
@@ -1,13 +1,17 @@
 # installer_skimage.sh will install the necessary packages to run
 # Where U At Plants? in opencv mode.
 
+# create a virtual enviornment
+VENV="wuap"
+pip3 install virtualenv
+mkvirtualenv $VENV
+workon $VENV
+
 # Define packages to install
 BUILDPKGS="cmake build-essential pkg-config"
 DEPENDENCIES="libgtk2.0-dev libtbb-dev libjasper-dev libjpeg-dev libpng-dev libtiff-dev libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libdc1394-22-dev libv4l-dev libatlas-base-dev libgstreamer1.0-0"
 PYTHONPKGS="python-dev python3-dev libopencv-dev"
 PIPMODULES="numpy scipy picamera opencv-python"
-
-VENV="wuap"
 
 # Update firmware
 apt-get update
@@ -15,9 +19,6 @@ apt-get upgrade -y
 
 # Install packages
 apt-get install $PYTHONPKGS -y
-pip3 install virtualenv
-mkvirtualenv $VENV
-workon $VENV
 pip3 install $PIPMODULES
 
 # clean installation
--- installer_skimage.sh
+++ installer_skimage.sh
@@ -1,12 +1,16 @@
 # installer_skimage.sh will install the necessary packages to run
 # Where U At Plants? in scikit-image mode.
 
+# create a virtual enviornment
+VENV="wuap-sk"
+pip3 install virtualenv
+mkvirtualenv $VENV
+workon $VENV
+
 # Define packages to install
 PACKAGES="python3-dev python3-pip"
 DEPENDENCIES="libgtk2.0-dev libtbb-dev libjasper-dev libjpeg-dev libpng-dev libtiff5-dev libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libdc1394-22-dev libv4l-dev libatlas-base-dev"
 PIPMODULES="numpy scipy scikit-image picamera"
-
-VENV="wuap-sk"
 
 # Update firmware
 apt-get update
@@ -14,9 +18,6 @@ apt-get upgrade -y
 
 # Install packages
 apt-get install $PACKAGES $DEPENENCIES -y
-pip3 install virtualenv
-mkvirtualenv $VENV
-workon $VENV
 pip3 install $PIPMODULES
 
 # clean installation
