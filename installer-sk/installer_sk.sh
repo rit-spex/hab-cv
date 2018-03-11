e3419d145fc8ef4bdc97e9e4e672094cd633f5d5
runphilrun <pjl7651@rit.edu>
2018-03-11 13:52:43 -0700

    cleanup repo

---
+++ installer-sk/installer_sk.sh
@@ -0,0 +1,18 @@
+# installer_skimage.sh will install the necessary packages to run
+# Where U At Plants? in scikit-image mode.
+
+# Define packages to install
+PACKAGES="python3-dev python3-pip"
+DEPENDENCIES="libgtk2.0-dev libtbb-dev libjasper-dev libjpeg-dev libpng-dev libtiff5-dev libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libdc1394-22-dev libv4l-dev libatlas-base-dev"
+PIPMODULES="numpy scipy scikit-image picamera"
+
+# Update firmware
+apt-get update
+apt-get upgrade -y
+
+# Install packages
+apt-get install $PACKAGES $DEPENENCIES -y
+pip3 install $PIPMODULES
+
+# clean installation
+apt autoremove -y
---
+++ installer/installer.sh
@@ -0,0 +1,19 @@
+# installer_skimage.sh will install the necessary packages to run
+# Where U At Plants? in opencv mode.
+
+# Define packages to install
+BUILDPKGS="cmake build-essential pkg-config"
+DEPENDENCIES="libgtk2.0-dev libtbb-dev libjasper-dev libjpeg-dev libpng-dev libtiff-dev libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libdc1394-22-dev libv4l-dev libatlas-base-dev libgstreamer1.0-0"
+PYTHONPKGS="python-dev python3-dev libopencv-dev"
+PIPMODULES="numpy scipy picamera opencv-python"
+
+# Update firmware
+apt-get update
+apt-get upgrade -y
+
+# Install packages
+apt-get install $DEPENDENCIES $PYTHONPKGS -y
+pip3 install $PIPMODULES
+
+# clean installation
+apt autoremove -y
