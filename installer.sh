aabf0c47d1eb8ccb9712f25d2157cbb807025ab8
runphilrun <pjl7651@rit.edu>
2018-03-10 17:36:19 -0800

    fix separate bootinto and installer scripts
    Former-commit-id: e7c46cb2b2310b22816175e013877b77b81add80

--- installer.sh
+++ installer.sh
@@ -3,9 +3,9 @@
 
 # Define packages to install
 BUILDPKGS="cmake build-essential pkg-config"
-DEPENDENCIES="libgtk2.0-dev libtbb-dev libjasper-dev libjpeg-dev libpng-dev libtiff-dev libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libdc1394-22-dev libv4l-dev libatlas-base-dev"
-PYTHONPKGS="python-dev python3-dev libopencv-dev python-opencv"
-PIPMODULES="numpy scipy picamera"
+DEPENDENCIES="libgtk2.0-dev libtbb-dev libjasper-dev libjpeg-dev libpng-dev libtiff-dev libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libdc1394-22-dev libv4l-dev libatlas-base-dev libgstreamer1.0-0"
+PYTHONPKGS="python-dev python3-dev libopencv-dev"
+PIPMODULES="numpy scipy picamera opencv-python"
 
 # Update firmware
 apt-get update
@@ -14,3 +14,6 @@ apt-get upgrade -y
 # Install packages
 apt-get install $PYTHONPKGS -y
 pip3 install $PIPMODULES
+
+# clean installation
+apt autoremove
