d17e48e524ac9a39990945301776cf6d3685dc3a
runphilrun <pjl7651@rit.edu>
2018-03-11 14:15:57 -0700

    absolute filepath

--- installer-sk/bootintowuap_sk.sh
+++ installer-sk/bootintowuap_sk.sh
@@ -3,5 +3,5 @@ rm /etc/systemd/system/wuap.service
 rm /etc/systemd/system/wuap_sk.service
 
 # enable boot service to start on reboot
-cp wuap_skimage.service /etc/systemd/system/wuap_sk.service
+cp wuap_sk.service /etc/systemd/system/wuap_sk.service
 systemctl enable wuap_sk.service
